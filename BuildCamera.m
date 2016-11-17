56230b70f10948a6ba88855d1b0173cda180a6b1
Marcelo Gennari <marcelogennari@outlook.com>
2016-11-17 11:57:21 +0000

    Talking to the demonstrators, they convinced me that the code is right. The fact that in the picture the lines of the cube are not parallel is due to projective geometry. If we take a picture of a highway for exmaple, even though the borders are parallel, they will look like they are not in the picture, because the points nearer to the viewer seem bigger than the ones in a certain distance. Apparently this code works fine and is good to go.

--- BuildCamera.m
+++ BuildCamera.m
@@ -5,15 +5,15 @@ function [KMatrix, CameraHeight, CameraWidth ] = BuildCamera( )
 
 %  Defining the characteristics of the Camera:
 ChipSize = [300; 300];
-FocalLength = 20;
+FocalLength = 35;
 EffectiveSize = [0.1; 0.1];
 Skewness = 0;
 PointsOffset = [0.5;0.5];
 
 % Getting the K Matrix and returing the camera heights and width
 KMatrix = CameraModel(ChipSize,FocalLength,EffectiveSize,Skewness,PointsOffset);
-CameraHeight = 10;
-CameraWidth = 10;
+CameraHeight = 300;
+CameraWidth = 300;
 
 end
 
--- LetMeTakeASelfie.m
+++ LetMeTakeASelfie.m
@@ -4,7 +4,30 @@ function [] = LetMeTakeASelfie( ObjectLines, T_ow, KMatrix, CameraHeight, Camera
 %Extrinsic parts of the camera model
 
 %% INPUT HYGIENE
+% Checking the ObjectLines matrix. It should be a 4x2n matrix:
+s = size(ObjectLines);
+if s(1) ~= 4 || mod(s(2),2) ~= 0
+   error('ObjectLines is not a valid matrix')
+end
 
+% Checking the transformation Matrices, which should be a 4x4
+s = size(T_ow);
+if s(1) ~= 4 || s(2) ~= 4
+    error('T_ow has an invalid size');
+end
+
+s = size(T_cw);
+if s(1) ~= 4 || s(2) ~= 4
+    error('T_cw has an invalid size');
+end
+
+% Checking the KMatrix. It should be a 3x3 Matrix as specified in the
+% CameraModel.m function
+
+s = size(KMatrix);
+if s(1)~= 3 || s(2) ~= 3
+   error('The Camera Model is not valid'); 
+end
 
 
 %% Transformation of Camera and Object:
@@ -18,8 +41,15 @@ ObjectLines = T_cw\ObjectLines;
 % Projects out the 4th "dimension" and multiplies with intrinsic component
 ObjectLines = KMatrix * ObjectLines(1:3,:);
 
+%Normalizing
+s = size(ObjectLines);
+for j = 1:s(2)
+   ObjectLines(1:2,j)  = ObjectLines(1:2,j)/ObjectLines(3,j);
+end
+
 plot(ObjectLines(1,:), ObjectLines(2,:));
 axis ij
+axis([0 CameraWidth 0 CameraHeight])
 title('Picture taken by the camera');
 
 end
